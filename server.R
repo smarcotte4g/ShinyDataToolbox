9b69193dec325f0a7a1c7c884c4e04c4c5355121
smarcotte4g <smarcotte4g@gmail.com>
2016-11-16 12:09:17 -0700

    Remove Blank Columns Test

--- server.R
+++ server.R
@@ -1,5 +1,5 @@
 library(shiny)
-
+options(shiny.maxRequestSize=30*1024^2)
 function(input, output) {
   datasetInput <- reactive({
     inFile <- input$file1
@@ -7,13 +7,12 @@ function(input, output) {
     if (is.null(inFile))
       return(NULL)
     
-    read.csv(inFile$datapath, header=T, sep=',', 
-             quote='"')
-    
+    mydf<- read.csv(inFile$datapath, header=T, sep=',', quote='"')
+    mydf[!sapply(mydf, function(x) all(is.na(x)))]
   })
-  output$contents <- renderTable({
-    datasetInput()
-  })
+  #output$contents <- renderTable({
+    #datasetInput()
+  #})
   
   output$downloadData <- downloadHandler(
     filename = function() { as.character(input$file1) },
