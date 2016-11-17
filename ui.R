537874770f7d61afe8cca4e8d56fd34a8e325f27
joshfullmer <joshfullmer@me.com>
2016-11-17 08:15:10 -0700

    Add UI + indenting

--- ui.R
+++ ui.R
@@ -2,32 +2,34 @@
 library(shiny)
 
 navbarPage("Uploading Files",
-   tabPanel("Remove Blank Columns",
-      sidebarLayout(
-         sidebarPanel(
-      fileInput('file1', 'Choose CSV File',
-         accept=c('text/csv', 
-                  'text/comma-separated-values,text/plain', 
-                  '.csv')),
-      tags$hr(),
-      downloadButton('downloadData1', 'Download')
-      ),
-      mainPanel(
-      tableOutput('contents1')
-      ))
-   ),
-   tabPanel("Salesforce Cleanup",
-            sidebarLayout(
-               sidebarPanel(
-                  fileInput('file2', 'Choose CSV File',
-                            accept=c('text/csv', 
-                                     'text/comma-separated-values,text/plain', 
-                                     '.csv')),
-                  tags$hr(),
-                  downloadButton('downloadData2', 'Download')
-               ),
-               mainPanel(
-                  tableOutput('contents2')
-               ))
-   )
+           tabPanel("Remove Blank Columns",
+                    sidebarLayout(
+                       sidebarPanel(
+                          fileInput('file1', 'Choose CSV File (Remove)',
+                                    accept=c('text/csv', 
+                                             'text/comma-separated-values,text/plain', 
+                                             '.csv')),
+                          tags$hr(),
+                          downloadButton('downloadData1', 'Download')
+                       ),
+                       mainPanel(
+                          tableOutput('contents1')
+                       ))
+           ),
+           navbarMenu("More...",
+                      tabPanel("Salesforce Cleanup",
+                               sidebarLayout(
+                                  sidebarPanel(
+                                     fileInput('file2', 'Choose CSV File (Salesforce)',
+                                               accept=c('text/csv', 
+                                                        'text/comma-separated-values,text/plain', 
+                                                        '.csv')),
+                                     tags$hr(),
+                                     downloadButton('downloadData2', 'Download')
+                                  ),
+                                  mainPanel(
+                                     tableOutput('contents2')
+                                  ))
+                      )
+           )
 )
