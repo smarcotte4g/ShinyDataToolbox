e1eb5cf2d12c6563bb71432c431fb3437e43aa49
joshfullmer <joshfullmer@me.com>
2016-11-17 11:00:46 -0700

    Add code blocks for each tab.

--- server.R
+++ server.R
@@ -24,15 +24,141 @@ function(input, output) {
       }
    )
    
+   ### CSV Splitter (slow)
+   
+   slowSplitInput <- reactive({
+      inFile <- input$slowSplitFile
+
+      if (is.null(inFile))
+         return(NULL)
+      
+      # <CODE FOR SPLITTING CSVS, HANDLING NEW LINES>
+      # <RETURN LIST OF DATA FRAMES>
+   })
+   
+   output$slowSplitContent <- renderTable({
+      slowSplitInput()
+   })
+   
+   output$slowSplitDownload <- downloadHandler(
+      filename = function() { as.character(input$slowSplitFile) },
+      content = function(file) {
+         write.csv(slowSplitInput(), file)
+      }
+   )
+   
    ### CSV Splitter (fast)
    
-   ### CSV Splitter (slow)
+   fastSplitInput <- reactive({
+      inFile <- input$fastSplitFile
+
+      if (is.null(inFile))
+         return(NULL)
+      
+      # <CODE FOR SPLITTING CSVS, NOT HANDLING NEW LINES>
+      # <RETURN LIST OF DATA FRAMES>
+   })
+   
+   output$fastSplitContent <- renderTable({
+      fastSplitInput()
+   })
+   
+   output$fastSplitDownload <- downloadHandler(
+      filename = function() { as.character(input$fastSplitFile) },
+      content = function(file) {
+         write.csv(fastSplitInput(), file)
+      }
+   )
    
    ### Salesforce Cleaner
    
+   salesforceInput <- reactive({
+      inFile <- input$salesforceFile
+      
+      if (is.null(inFile))
+         return(NULL)
+      
+      # <CODE FOR CLEANING UP SALESFORCE>
+      # <RETURN LIST OF DATA FRAMES, BASED ON CHECKBOXES>
+   })
+   
+   output$salesforceContent <- renderTable({
+      salesforceInput()
+   })
+   
+   output$salesforceDownload <- downloadHandler(
+      filename = function() { as.character(input$salesforceFile) },
+      content = function(file) {
+         write.csv(salesforceInput(), file)
+      }
+   )
+   
    ### Filesmasher
+   
+   filesmasherInput <- reactive({
+      inFile <- input$filesmasherFile
+      
+      if (is.null(inFile))
+         return(NULL)
+      
+      # <CODE FOR SMASHING CSVS TOGETHER (ROW BIND)>
+      # <RETURN A DATA FRAME>
+   })
+   
+   output$filesmasherContent <- renderTable({
+      filesmasherInput()
+   })
+   
+   output$filesmasherDownload <- downloadHandler(
+      filename = function() { as.character(input$filesmasherFile) },
+      content = function(file) {
+         write.csv(filesmasherInput(), file)
+      }
+   )
    
    ### CSV Merge
    
+   csvMergeInput <- reactive({
+      inFile <- input$csvMergeFile
+      
+      if (is.null(inFile))
+         return(NULL)
+      
+      # <CODE FOR MERGING CSVS BY ID NUMBER>
+      # <RETURN A DATA FRAME>
+   })
+   
+   output$csvMergeContent <- renderTable({
+      csvMergeInput()
+   })
+   
+   output$csvMergeDownload <- downloadHandler(
+      filename = function() { as.character(input$csvMergeFile) },
+      content = function(file) {
+         write.csv(csvMergeInput(), file)
+      }
+   )
+   
    ### CSV Converter
+   
+   csvConverterInput <- reactive({
+      inFile <- input$csvConverterFile
+      
+      if (is.null(inFile))
+         return(NULL)
+      
+      # <CODE FOR CONVERTING FILES TO CSV, HANDLING JSON, XML, AND YAML>
+      # <RETURN A DATA FRAME>
+   })
+   
+   output$csvConverterContent <- renderTable({
+      csvConverterInput()
+   })
+   
+   output$csvConverterDownload <- downloadHandler(
+      filename = function() { as.character(input$csvConverterFile) },
+      content = function(file) {
+         write.csv(csvConverterInput(), file)
+      }
+   )
 }
