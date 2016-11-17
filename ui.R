bdae221b0cccc41d0497221363360dd9128b65eb
joshfullmer <joshfullmer@me.com>
2016-11-17 11:14:38 -0700

    Rearrange layout

--- ui.R
+++ ui.R
@@ -4,15 +4,15 @@ navbarPage("Uploading Files",
            tabPanel("Remove Blank Columns",
                     sidebarLayout(
                        sidebarPanel(
+                          p("This function will remove all blank columns contained in a CSV file.")
+                       ),
+                       mainPanel(
                           fileInput('blankColumnsFile', 'Choose CSV File (Remove)',
                                     accept=c('text/csv', 
                                              'text/comma-separated-values,text/plain', 
                                              '.csv')),
                           tags$hr(),
-                          downloadButton('blankColumnsDownload', 'Download')
-                       ),
-                       mainPanel(
-                          p("This function will remove all blank columns contained in a CSV file."),
+                          downloadButton('blankColumnsDownload', 'Download'),
                           tableOutput('blankColumnsContent')
                        ))
            ),
@@ -20,28 +20,30 @@ navbarPage("Uploading Files",
                       tabPanel("Contains New Lines",
                                sidebarLayout(
                                   sidebarPanel(
+                                     p("This function will remove all blank columns contained in a CSV file.")
+                                  ),
+                                  mainPanel(
                                      fileInput('slowSplitFile', 'Choose CSV File (Salesforce)',
                                                accept=c('text/csv', 
                                                         'text/comma-separated-values,text/plain', 
                                                         '.csv')),
                                      tags$hr(),
-                                     downloadButton('slowSplitDownload', 'Download')
-                                  ),
-                                  mainPanel(
+                                     downloadButton('slowSplitDownload', 'Download'),
                                      tableOutput('slowSplitContent')
                                   ))
                       ),
                       tabPanel("Doesn't Contain New Lines",
                                sidebarLayout(
                                   sidebarPanel(
+                                     p("This function will remove all blank columns contained in a CSV file.")
+                                  ),
+                                  mainPanel(
                                      fileInput('fastSplitFile', 'Choose CSV File (Salesforce)',
                                                accept=c('text/csv', 
                                                         'text/comma-separated-values,text/plain', 
                                                         '.csv')),
                                      tags$hr(),
-                                     downloadButton('fastSplitDownload', 'Download')
-                                  ),
-                                  mainPanel(
+                                     downloadButton('fastSplitDownload', 'Download'),
                                      tableOutput('fastSplitContent')
                                   ))
                       )
@@ -49,56 +51,60 @@ navbarPage("Uploading Files",
            tabPanel("Salesforce Cleaner",
                     sidebarLayout(
                        sidebarPanel(
+                          p("This function will remove all blank columns contained in a CSV file.")
+                       ),
+                       mainPanel(
                           fileInput('salesforceFile', 'Choose CSV File (Remove)',
                                     accept=c('text/csv', 
                                              'text/comma-separated-values,text/plain', 
                                              '.csv')),
                           tags$hr(),
-                          downloadButton('salesforceDownload', 'Download')
-                       ),
-                       mainPanel(
+                          downloadButton('salesforceDownload', 'Download'),
                           tableOutput('salesforceContent')
                        ))
            ),
            tabPanel("Filesmasher",
                     sidebarLayout(
                        sidebarPanel(
+                          p("This function will remove all blank columns contained in a CSV file.")
+                       ),
+                       mainPanel(
                           fileInput('filesmasherFile', 'Choose CSV File (Remove)',
                                     accept=c('text/csv', 
                                              'text/comma-separated-values,text/plain', 
                                              '.csv')),
                           tags$hr(),
-                          downloadButton('filesmasherDownload', 'Download')
-                       ),
-                       mainPanel(
+                          downloadButton('filesmasherDownload', 'Download'),
                           tableOutput('filesmasherContent')
                        ))
            ),
            tabPanel("CSV Merge",
                     sidebarLayout(
                        sidebarPanel(
+                          p("This function will remove all blank columns contained in a CSV file.")
+                       ),
+                       mainPanel(
                           fileInput('csvMergeFile', 'Choose CSV File (Remove)',
                                     accept=c('text/csv', 
                                              'text/comma-separated-values,text/plain', 
                                              '.csv')),
                           tags$hr(),
-                          downloadButton('csvMergeDownload', 'Download')
-                       ),
-                       mainPanel(
+                          downloadButton('csvMergeDownload', 'Download'),
                           tableOutput('csvMergeContent')
                        ))
            ),
            tabPanel("CSV Converter",
                     sidebarLayout(
                        sidebarPanel(
+                          p("This function will remove all blank columns contained in a CSV file.")
+                       ),
+                       mainPanel(
                           fileInput('csvConverterFile', 'Choose CSV File (Remove)',
                                     accept=c('text/csv', 
                                              'text/comma-separated-values,text/plain', 
                                              '.csv')),
                           tags$hr(),
-                          downloadButton('csvConverterDownload', 'Download')
-                       ),
-                       mainPanel(
+                          downloadButton('csvConverterDownload', 'Download'),
                           tableOutput('csvConverterContent')
                        ))
            )
