00e19433f25645ec6999a9ec49da06224ea24dc0
joshfullmer <joshfullmer@me.com>
2016-11-17 08:20:51 -0700

    Merge to master

--- server.R
+++ server.R
@@ -1,7 +1,6 @@
 library(shiny)
 options(shiny.maxRequestSize=30*1024^2)
 function(input, output) {
-<<<<<<< HEAD
    datasetInput <- reactive({
       inFile <- input$file1
       print(inFile)
@@ -22,25 +21,4 @@ function(input, output) {
          write.csv(datasetInput(), file)
       }
    ) 
-=======
-  datasetInput <- reactive({
-    inFile <- input$file1
-    
-    if (is.null(inFile))
-      return(NULL)
-    
-    mydf<- read.csv(inFile$datapath, header=T, sep=',', quote='"')
-    mydf[!sapply(mydf, function(x) all(is.na(x)|x==""))] #remove all of the blank columns
-  })
-  #output$contents <- renderTable({
-    #datasetInput()
-  #})
-  
-  output$downloadData <- downloadHandler(
-    filename = function() { as.character(input$file1) },
-    content = function(file) {
-      write.csv(datasetInput(), file)
-    }
-  ) 
->>>>>>> master
 }
